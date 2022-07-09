22fc3340505f38101fff1f0fe0fc22a2e9cb09f9
tectumor <72674429+tectumor@users.noreply.github.com>
2022-07-08 23:38:56 -0500

    remove button styles

--- components/mintComp/MintButtonTauros.js
+++ components/mintComp/MintButtonTauros.js
@@ -6,97 +6,9 @@ import { useDispatch, useSelector } from "react-redux"
 import { connect } from "@/src/redux2/blockchain/blockchainActions"
 import { fetchData } from "@/src/redux2/data/dataActions"
 import * as s from "@/src/styles/globalStyles"
-import styled from "styled-components"
 
 const truncate = (input, len) =>
   input.length > len ? `${input.substring(0, len)}...` : input;
-
-  export const StyledButton = styled.button`
-  padding: 10px;
-  border-radius: 50px;
-  border: none;
-  background-color: var(--secondary);
-  padding: 10px;
-  font-weight: bold;
-  color: var(--secondary-text);
-  width: 100px;
-  cursor: pointer;
-  box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
-  -webkit-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
-  -moz-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
-  :active {
-  box-shadow: none;
-  -webkit-box-shadow: none;
-  -moz-box-shadow: none;
-  }
-`;
-
-export const StyledRoundButton = styled.button`
-  padding: 10px;
-  border-radius: 100%;
-  border: none;
-  background-color: var(--primary);
-  padding: 10px;
-  font-weight: bold;
-  font-size: 15px;
-  color: var(--primary-text);
-  width: 30px;
-  height: 30px;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-shadow: 0px 4px 0px -2px rgba(250, 250, 250, 0.3);
-  -webkit-box-shadow: 0px 4px 0px -2px rgba(250, 250, 250, 0.3);
-  -moz-box-shadow: 0px 4px 0px -2px rgba(250, 250, 250, 0.3);
-  :active {
-    box-shadow: none;
-    -webkit-box-shadow: none;
-    -moz-box-shadow: none;
-  }
-`;
-
-export const ResponsiveWrapper = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  justify-content: stretched;
-  align-items: stretched;
-  width: 100%;
-  @media (min-width: 767px) {
-    flex-direction: row;
-  }
-`;
-
-export const StyledLogo = styled.img`
-  width: 1000px;
-  @media (min-width: 767px) {
-    width: 1000px;
-  }
-  transition: width 0.5s;
-  transition: height 0.5s;
-`;
-
-export const StyledImg = styled.img`
-  box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
-  border: 4px dashed var(--secondary);
-  background-color: var(--accent);
-  border-radius: 100%;
-  width: 200px;
-  @media (min-width: 900px) {
-    width: 250px;
-  }
-  @media (min-width: 1000px) {
-    width: 300px;
-  }
-  transition: width 0.5s;
-`;
-
-export const StyledLink = styled.a`
-  color: var(--secondary);
-  text-decoration: none;
-`;
-
 
 export default function MintButtonTauros({ user }) {
     const dispatch = useDispatch();
@@ -221,7 +133,7 @@ export default function MintButtonTauros({ user }) {
             }}
           >
 
-            <StyledButton
+            <Button
               style={{
                 margin: "5px",
               }}
@@ -230,7 +142,7 @@ export default function MintButtonTauros({ user }) {
               }}
             >
               {CONFIG.MARKETPLACE}
-            </StyledButton>
+            </Button>
           </span>          
       </s.Container>              
   
@@ -248,7 +160,7 @@ export default function MintButtonTauros({ user }) {
           
           
           
-      <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
+      <Flex flex={1} style={{ padding: 24 }} test>
         {<s.Container flex={1} jc={"center"} ai={"center"}>
           {/* <StyledImg alt={"example"} src={"/config/images/comp1_1.gif"} /> */}
         </s.Container> }
@@ -275,9 +187,9 @@ export default function MintButtonTauros({ user }) {
               color: "var(--primary-text)",
             }}
           >
-            <StyledLink target={"_blank"} href={CONFIG.SCAN_LINK}>
+            <Link target={"_blank"} href={CONFIG.SCAN_LINK}>
               {truncate(CONFIG.CONTRACT_ADDRESS, 15)}
-            </StyledLink>
+            </Link>
           </s.TextDescription>
 
           <s.SpacerSmall />
@@ -312,7 +224,7 @@ export default function MintButtonTauros({ user }) {
               blockchain.smartContract === null ? (
                 <s.Container ai={"center"} jc={"center"}>
                   <s.SpacerSmall />
-                  <StyledButton
+                  <Button
                     onClick={(e) => {
                       e.preventDefault();
                       dispatch(connect());
@@ -320,7 +232,7 @@ export default function MintButtonTauros({ user }) {
                     }}
                   >
                     CONNECT
-                  </StyledButton>
+                  </Button>
                   {blockchain.errorMsg !== "" ? (
                     <>
                       <s.SpacerSmall />
@@ -347,7 +259,7 @@ export default function MintButtonTauros({ user }) {
                   </s.TextDescription>
                   <s.SpacerMedium />
                   <s.Container ai={"center"} jc={"center"} fd={"row"}>
-                    <StyledRoundButton
+                    <Button
                       style={{ lineHeight: 0.4,
                              color: "var(--secondary-text)"
                             }}
@@ -358,7 +270,7 @@ export default function MintButtonTauros({ user }) {
                       }}
                     >
                       -
-                    </StyledRoundButton>
+                    </Button>
                     <s.SpacerMedium />
                     <s.TextDescription
                       style={{
@@ -369,7 +281,7 @@ export default function MintButtonTauros({ user }) {
                       {mintAmount}
                     </s.TextDescription>
                     <s.SpacerMedium />
-                    <StyledRoundButton
+                    <Button
                       style={{ 
                              color: "var(--secondary-text)"
                             }}
@@ -380,11 +292,11 @@ export default function MintButtonTauros({ user }) {
                       }}
                     >
                       +
-                    </StyledRoundButton>
+                    </Button>
                   </s.Container>
                   <s.SpacerSmall />
                   <s.Container ai={"center"} jc={"center"} fd={"row"}>
-                    <StyledButton
+                    <Button
                       disabled={claimingNft ? 1 : 0}
                       onClick={(e) => {
                         e.preventDefault();
@@ -393,7 +305,7 @@ export default function MintButtonTauros({ user }) {
                       }}
                     >
                       {claimingNft ? "Minting" : "MINT"}
-                    </StyledButton>
+                    </Button>
                   </s.Container>
                 </>
               )}
@@ -409,7 +321,7 @@ export default function MintButtonTauros({ user }) {
             style={{ transform: "scaleX(-1)" }}
           /> */}
         </s.Container>
-      </ResponsiveWrapper>
+      </Flex>
       <s.SpacerMedium />
 
     </s.Container>
