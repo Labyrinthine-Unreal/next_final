9f0d2551206468d7eada88907eb9f37703a73064
Calvin Green <calvingreen94@aol.com>
2022-07-16 20:49:34 -0500

    Update MintButtonTaurosV2.js

--- components/mintComp/MintButtonTaurosV2.js
+++ components/mintComp/MintButtonTaurosV2.js
@@ -1,4 +1,4 @@
-import { Container,SimpleGrid,Divider,useToast,Flex,NumberInputStepper,NumberIncrementStepper,NumberDecrementStepper,NumberInputField,Text,NumberInput,Link,Box,Tabs,TabPanel,TabList,Tab,TabPanels,FormControl,FormLabel,Input } from "@chakra-ui/react"
+import { Container, SimpleGrid, Divider, useToast, Flex, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, NumberInputField, Text, NumberInput, Link, Box, Tabs, TabPanel, TabList, Tab, TabPanels, FormControl, FormLabel, Input } from "@chakra-ui/react"
 import { useEffect, useState, } from "react";
 // import { useDispatch, useSelector } from "react-redux"
 // import { connect } from "@/src/redux/blockchain/blockchainActions"
@@ -6,135 +6,97 @@ import { useEffect, useState, } from "react";
 import { Button } from 'web3uikit';
 import { useMoralis, useWeb3ExecuteFunction } from 'react-moralis';
 import taurosABI from "../ABIs/taurosABI"
-
+import CustomContainer from "@components/CustomContainer";
 
 const truncate = (input, len) =>
-    input.length > len ? `${input.substring(0, len)}...` : input;
+  input.length > len ? `${input.substring(0, len)}...` : input;
 
 export default function MBT() {
-    // const dispatch = useDispatch();
-    // const blockchain = useSelector((state) => state.blockchain);
-    // const data = useSelector((state) => state.data);
-    // const [claimingNft, setClaimingNft] = useState(false);
-    // const [feedback, setFeedback] = useState(false);
-    // const [mintAmount, setMintAmount] = useState(1);
-    // const [CONFIG, SET_CONFIG] = useState({
-    //     CONTRACT_ADDRESS: "",
-    //     SCAN_LINK: "",
-    //     NETWORK: {
-    //         NAME: "",
-    //         SYMBOL: "",
-    //         ID: 0,
-    //     },
-    //     NFT_NAME: "",
-    //     SYMBOL: "",
-    //     MAX_SUPPLY: 1,
-    //     WEI_COST: 0,
-    //     DISPLAY_COST: 0,
-    //     GAS_LIMIT: 0,
-    //     MARKETPLACE: "",
-    //     MARKETPLACE_LINK: "",
-    //     SHOW_BACKGROUND: false,
-    // });
-    
-    // const decrementMintAmount = () => {
-    //     let newMintAmount = mintAmount - 1;
-    //     if (newMintAmount < 1) {
-    //         newMintAmount = 1;
-    //     }
-    //     setMintAmount(newMintAmount);
-    // };
 
-    // const incrementMintAmount = () => {
-    //     let newMintAmount = mintAmount + 1;
-    //     if (newMintAmount > 10) {
-    //         newMintAmount = 10;
-    //     }
-    //     setMintAmount(newMintAmount);
-    // };
+  const [amount, setAmount] = useState(0)
+  const handleChange = (value) => setAmount(value)
+  const toast = useToast()
 
-    // const getData = () => {
-    //     if (blockchain.account !== "" && blockchain.smartContract !== null) {
-    //         dispatch(fetchData(blockchain.account));
-    //     }
-    // };
+  const { authenticate, isAuthenticated, isAuthenticating, Moralis, user, account, logout } = useMoralis();
+  const contractProcessor = useWeb3ExecuteFunction();
+  // const [value, setValue] = useControllableState({ defaultValue: 1 })
+  useEffect(() => {
+    if (isAuthenticated) {
 
-    // const getConfig = async () => {
-    //     const configResponse = await fetch("../public/config/config.json", {
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             Accept: "application/json",
-    //         },
-    //     });
-    //     const config = await configResponse.json();
-    //     SET_CONFIG(config);
-    // };
-
-    // useEffect(() => {
-    //     getConfig();
-    // }, []);
-
-    // useEffect(() => {
-    //     getData();
-    // }, [blockchain.account])
-
-    const [amount, setAmount] = useState(0)
-    const [receiver,setReceiver ] = useState('')
-    const handleChange = (value) => setAmount(value)
-    const toast = useToast()
-
-    const { authenticate, isAuthenticated, isAuthenticating, Moralis, user, account, logout } = useMoralis();
-    const contractProcessor = useWeb3ExecuteFunction();
-
-    useEffect(() => {
-        if (isAuthenticated) {
-
-        }
-
-    }, [isAuthenticated])
-
-    async function _mintTauros() {
-        let options = {
-          contractAddress: '0x7ed80F34Bf90Eff85423D88b7B827b75A51B6552',
-          functionName: 'mintNFTs',
-          abi: taurosABI,
-          msgValue: Moralis.Units.ETH("0.05"),
-          params: {
-            _count: 1,
-          }
-        }
-        await Moralis.enableWeb3()
-        await contractProcessor.fetch({
-          params: options,
-          onSuccess: () => {
-            toast({
-                title: 'Mint Successful',
-                description: "Minted TaurosDAO Membership",
-                status: 'success',
-                duration: 9000,
-                isClosable: true,
-              })
-            console.log("Mint successful");
-        },
-        onError: (error) => {
-            toast({
-                title: 'Mint Failed.. Not enough Ether To Purchase TaurosDAO Membership',
-                description: console.log(error),
-                status: "error",
-                duration: '9000',
-                isClosable: true
-            }) 
-            console.log(error);
-        }
-        })
-      }
-    return (
-        
-        <Button style={{width: "140px", height: "40px", fontSize: "16px", border: "none", borderRadius: "6px"}} onClick={() => {
-            if (isAuthenticated) {_mintTauros();}
-          }} text={"Mint Taurus"} theme={"primary"} />
-      );
     }
+
+  }, [isAuthenticated])
+
+  async function _mintTauros() {
+    let options = {
+      contractAddress: '0x7ed80F34Bf90Eff85423D88b7B827b75A51B6552',
+      functionName: 'mintNFTs',
+      abi: taurosABI,
+      msgValue: Moralis.Units.ETH("0.05") * amount,
+      params: {
+        _count: amount,
+      }
+    }
+
+    await Moralis.enableWeb3()
+    await contractProcessor.fetch({
+      params: options,
+      onSuccess: () => {
+        toast({
+          title: 'Mint Successful',
+          description: "Minted TaurosDAO Membership",
+          status: 'success',
+          duration: 9000,
+          isClosable: true,
+        })
+        console.log("Mint successful");
+      },
+      onError: (error) => {
+        toast({
+          title: 'Mint Failed.. Not enough Ether To Purchase TaurosDAO Membership',
+          description: console.log(error),
+          status: "error",
+          duration: '9000',
+          isClosable: true
+        })
+        console.log(error);
+      }
+    })
+  }
+  return (
+    <CustomContainer>
+      <Text fontSize="xl" fontWeight="bold">
+        <form onSubmit={async e => {
+          e.preventDefault()
+        }}>
+          <FormControl mt="4">
+            <FormLabel htmlFor="amount">
+              Amount to Mint
+            </FormLabel>
+            <NumberInput step={1} onChange={handleChange}>
+              <NumberInputField id="amount" value={amount} />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </FormControl>
+          <Button onClick={() => {
+            if (isAuthenticated) { _mintTauros(); }
+          }} text={"Mint Taurus"} theme={"primary"} />
+
+        </form>
+      </Text>
+    </CustomContainer>
+  )
+}
+//   return (
+
+//     <Button onClick={() => {
+//       if (isAuthenticated) { _mintTauros(); }
+//     }} text={"Mint Taurus"} theme={"primary"} />
+//   );
+// }
 
 {/* Opensea button --> move to bottom of the page */ }
 {/* <Container>
