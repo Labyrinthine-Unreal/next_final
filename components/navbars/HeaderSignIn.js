b6c3ed4a0ef4d6e38422cd573d345128780e016e
tectumor <72674429+tectumor@users.noreply.github.com>
2022-07-17 22:06:31 -0500

    clean up commented out code

--- components/navbars/HeaderSignIn.js
+++ components/navbars/HeaderSignIn.js
@@ -1,73 +1,33 @@
-import { useMoralis } from "react-moralis"
-import { Flex, Center, Box, Button, IconButton, Text } from "@chakra-ui/react"
+import { Flex, Center, IconButton } from "@chakra-ui/react"
 import { ImMenu } from 'react-icons/im'
-import UserProfile from "./UserProfile"
-import Image from "next/image"
 import { ConnectButton } from "web3uikit"
 
 
 export default function HeaderSignIn({ onOpen, ...rest }) {
-  // const {isAuthenticated, authenticate, user, logout, isLoggingOut} =  useMoralis()
-  // console.log(isAuthenticated)
-  // if(!isAuthenticated){
 
     return (
-      <Flex
-        ml={{ base: 0, md: 60 }}
-        px="4"
-        top="0"
-        height="20"
-        zIndex="1"
-        alignItems="center"
-        borderBottom="1px"
-        borderBottomColor="gray.200"
-        justifyContent={{ base: "space-between", md: "flex-end" }}
-        {...rest}
-      >
-        <IconButton
-          display={{ base: "flex", md: "none" }}
-          onClick={onOpen}
-          variant="outline"
-          aria-label="open menu"
-          icon={<ImMenu />}
-        />
-        <Center>
-              <Box size={{ base: "sm", md: "md" }}>
-              <ConnectButton />
-                {/* <Button colorScheme="purple"
-                onClick={()=>authenticate({
-                  signingMessage:"Tauros SignIN"
-                })}>
-                  Metamask Login
-                </Button> */}
-              </Box>
-            </Center>
-      </Flex>
-    );
+          <Flex
+              ml={{ base: 0, md: 60 }}
+              px="4"
+              top="0"
+              height="20"
+              zIndex="1"
+              alignItems="center"
+              borderBottom="1px"
+              borderBottomColor="gray.200"
+              justifyContent={{ base: "space-between", md: "flex-end" }}
+              {...rest}
+            >
+              <IconButton
+                display={{ base: "flex", md: "none" }}
+                onClick={onOpen}
+                variant="outline"
+                aria-label="open menu"
+                icon={<ImMenu />}
+              />
+              <Center>
+                  <ConnectButton />
+              </Center>
+          </Flex>
+      );
   }
-
-//   return (
-//     <Flex
-//       ml={{ base: 0, md: 60 }}
-//       px="4"
-//       top="0"
-//       height="20"
-//       zIndex="1"
-//       alignItems="center"
-//       // bg="white"
-//       borderBottom="1px"
-//       borderBottomColor="gray.200"
-//       justifyContent={{ base: "space-between", md: "flex-end" }}
-//       {...rest}
-//     >
-//       <IconButton
-//         display={{ base: "flex", md: "none" }}
-//         onClick={onOpen}
-//         variant="outline"
-//         aria-label="open menu"
-//         icon={<ImMenu />}
-//       />
-//       <UserProfile user={user} logout={logout} isLoggingOut={isLoggingOut}/>
-//     </Flex>
-//   );
-// }
