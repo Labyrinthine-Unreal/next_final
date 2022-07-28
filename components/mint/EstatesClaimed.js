8d3e46bd394462bfe3c8b2e9bd7ac95c95a11713
Calvin Green <calvingreen94@aol.com>
2022-07-28 14:30:27 -0500

    Update EstatesClaimed.js

--- components/mint/EstatesClaimed.js
+++ components/mint/EstatesClaimed.js
@@ -34,7 +34,7 @@ export default function EstatesClaimed() {
                     }
                 }
                 let estatesOptions = {
-                    contractAddress: '0x075ba61CFA6437EF6531eE7beDEbCB0D18aa14C4',
+                    contractAddress: '0xf5D072526BBC7c312B7592FD9674600931A9e4D0',
                     functionName: 'balanceOf',
                     abi: estatesABI,
                     params: {
