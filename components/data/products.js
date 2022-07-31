457cae6232f0ba80b8c77956813561d401421073
tectumor <72674429+tectumor@users.noreply.github.com>
2022-07-31 15:16:42 -0500

    edit card => token

--- components/data/products.js
+++ components/data/products.js
@@ -2,13 +2,13 @@ const properties = [
     {
         key: 1,
         imageUrl: 'videos/TaurosCard.mp4',
-        imageAlt: 'TAUROS Card',
-        title: 'TAUROS MEMBERSHIP CARD',
+        imageAlt: 'TAUROS Token',
+        title: 'TAUROS MEMBERSHIP TOKEN',
         salePrice: 'Sale: 0.05 ETH',
         info: "ℹ",
         href: '/tauros',
         description: (<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>),
-        active: (<p style={{color: "#2E7DAF"}}>Minting is not active<br />Get Whitelisted below</p>)
+        active: (<p style={{color: "#2E7DAF"}}>Minting is not active</p>)
     },
     {
         key: 2,
