b2dcdf95651ad86b97cd55b28e3a8df030201643
Calvin Green <calvingreen94@aol.com>
2022-08-02 13:58:28 -0500

    Update MintButtonTaurosV2.js

--- components/mint/MintButtonTaurosV2.js
+++ components/mint/MintButtonTaurosV2.js
@@ -27,7 +27,7 @@ export default function MBT() {
 
   async function _mintTauros() {
     let options = {
-      contractAddress: '0x3272B82B04d15d44073638f8aC0d516EA2158E4F',
+      contractAddress: '0xBb97DE383d1E36c9063b7cFEb6C6DB2C4bC42d38',
       functionName: 'mintNFTs',
       abi: taurosABI,
       msgValue: Moralis.Units.ETH("0.05")* amount,
