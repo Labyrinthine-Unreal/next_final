1c23c683ee01c223d0ec1ae3ce065bca47d157e3
tectumor <72674429+tectumor@users.noreply.github.com>
2022-08-12 22:58:29 -0500

    remove duplicate customContainer, you win Calvin

--- components/mint/CustomContainer.js
+++
@@ -1,8 +0,0 @@
-import { Box } from "@chakra-ui/react"
-export default function CustomContainer({children}){
-    return(
-        <Box bg="white" width="full" height="full" pr={10} pv="10" rounded="lg" textAlign="left">
-            {children}
-        </Box>
-    )
-}
