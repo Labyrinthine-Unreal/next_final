e7fdda906d2adca1a002a70500dc63c569eec71a
tectumor <72674429+tectumor@users.noreply.github.com>
2022-08-13 20:42:44 -0500

    Merge branch 'to-publish' of https://github.com/Labyrinthine-Unreal/next_final into to-publish

--- pages/galleries.js
+++ pages/galleries.js
@@ -17,19 +17,25 @@ export default function GalleriesPage() {
             <Heading textStyle="title" fontSize="3xl">
                 <Galleries />
             </Heading>
-            
-            <Text textStyle="landingPageContent">
-                A nine week series of 3D digital art galleries in New Art City. Theme: <span style={{fontStyle: "italic"}}>Dante&apos;s Inferno</span>. New gallery floor is added every Monday. Some of the floors may hide a TAUROS token. Those who find one, may contact us on <Link href='https://discord.com/invite/labyrinthine' isExternal color="#2E7DAF">discord</Link> with their wallet address.
-            </Text>
 
-            <Box pt={10} align="center">
-                <SimpleGrid pl={{base: "1", md: "3", lg: "5"}} columns={{base: "1", md: "2", lg: "3"}} gap={5} mt="4" minH="full" spacingY={20}>
-                {galleriesData.map((gallery, i) => (
-                    <Flex key={i}>
-                        <GalleriesCard gallery={gallery} />
-                    </Flex>
-                ))}
-                </SimpleGrid>
+            <Box pb={20} textStyle="landingPageContent">
+                <Heading fontSize="large" textStyle="subtitle">
+                    DANTE&apos;S INFERNO
+                </Heading>
+            
+                <Text>
+                    A series of 3D digital art galleries in New Art City. Theme: <span style={{fontStyle: "italic"}}>Dante&apos;s Inferno</span>. New gallery floor is added every Monday. Some of the floors may hide a TAUROS token. Those who find one, may contact us on <Link href='https://discord.com/invite/labyrinthine' isExternal color="#2E7DAF">discord</Link> with their wallet address.
+                </Text>
+
+                <Box pt={10} align="center">
+                    <SimpleGrid pl={{base: "1", md: "3", lg: "5"}} columns={{base: "1", md: "2", lg: "3"}} gap={5} mt="4" minH="full" spacingY={20}>
+                    {galleriesData.map((gallery, i) => (
+                        <Flex key={i}>
+                            <GalleriesCard gallery={gallery} />
+                        </Flex>
+                    ))}
+                    </SimpleGrid>
+                </Box>
             </Box>
         </Box>
     );
