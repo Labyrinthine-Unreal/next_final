58494f4edfa3d45d80b565d82ee7d9029031b7e1
tectumor <72674429+tectumor@users.noreply.github.com>
2022-08-16 17:32:14 -0500

    edit tauros card types

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
             <Box textStyle="landingPageContent" mb={10} pb={0}>
                 <motion.div initial="initial" whileInView="whileInView" whileHover="hover" tap="whileTap">
                     <Collapse in={isOpen} startingHeight={60}>
-                        <Text>TaurosDAO is an exclusive community of artists and collectors founded in January 2022 by <Link href="https://www.labyrinthineunreal.io/" target="_blank" rel="noreferrer" style={{textDecoration: "none", color: "#2E7DAF", fontWeight: "400"}}>Labyrinthine Unreal</Link>. For purposes of governance and other membership privileges, members must hold at least one TAUROS token. There are two types of TAUROS tokens: <span style={{fontWeight: "400"}}>OG TAUROS</span> and <span style={{fontWeight: "400"}}>Standard</span>. OG members receive free airdrops of all future assets Labyrinthine Unreal and TaurosDAO produce (such as, Merca City Estates, Initiation Masks, etc). Standard members receive random airdrops and most other perks (for a more detailed overview check the members page).
+                        <Text>TaurosDAO is an exclusive community of artists and collectors founded in January 2022 by <Link href="https://www.labyrinthineunreal.io/" target="_blank" rel="noreferrer" style={{textDecoration: "none", color: "#2E7DAF", fontWeight: "400"}}>Labyrinthine Unreal</Link>. For purposes of governance and other membership privileges, members must hold at least one TAUROS Card. There are three types of Cards: <span style={{fontWeight: "400"}}>OG</span>, <span style={{fontWeight: "400"}}>Archon</span>, and <span style={{fontWeight: "400"}}>Guardian</span>. All OG Cards will receive an Estate. Afterwards, they have a 50% chance of receiving free airdrops of all future assets Labyrinthine Unreal and TaurosDAO produce. Archons and Guardians have a 25% and a 10% chance respectively. A part of the game tokens will be distributed to all members equally, regardless of type (for a more detailed overview of benefits and voting rights, check the <NextLink href="/members"><a style={{color: "#2E7DAF", fontWeight: "400"}}>members</a></NextLink> page).
                         </Text>
                         
                         <Text ml={6} mt={5} p={5} rounded="lg" brightness="90%" borderColor="teal.700" maxW={800} _hover={{color: "teal.600", bg: "#eee", brightness: "100%", boxShadow: "dark-lg", transition: "transform 0.15s ease-in-out", transform: "scale3d(1.05, 1.05, 1.05)",}} style={{fontWeight: "400"}}>To be able to mint TAUROS, you must save your Ethereum wallet address with the allowlist below when it goes live. We will announce the date soon, so stay tuned!
@@ -68,24 +68,6 @@ export default function Home() {
                         
                         <Text pt={5}><span style={{fontWeight: "400"}}>Merca City</span> is an open world Metaverse/RTS Game, where TaurosDAO members, artists, designers, developers, gamers, and collectors may come together to play, experiment, and collaborate. <NextLink href="/estates"><a style={{color: "#2E7DAF", fontWeight: "400"}}>Estates</a></NextLink> are Merca City&apos;s genesis assets. The Estates NFT is dynamic, A.K.A. dNFT or <em>smart NFT</em>; its metadata and output animation changes based on live weather conditions pinpointing to a secret location. It reports temperature and visibility, and its animation depicts four weather conditions: sunny, overcast, rainy, and snowy.
                         </Text>
-
-                        {/* <Text>TaurosDAO is an exclusive community of artists and collectors founded in January 2022 by <Link href="https://www.labyrinthineunreal.io/" target="_blank" rel="noreferrer" style={{fontWeight: "400"}}>Labyrinthine Unreal</Link>. For purposes of governance and other membership privileges, members must hold at least one TAUROS token. There will only ever be 2000 TAUROS tokens: <span style={{fontWeight: "400"}}>300 OG</span> and <span style={{fontWeight: "400"}}>1700 Standard</span>. OG members receive free airdrops of all future assets Labyrinthine Unreal and TaurosDAO produce (such as, Merca City Estates, Initiation Masks, etc). Standard members receive random airdrops and most other perks (for a more detailed overview check the members page).
-                        </Text>
-                        
-                        <Text ml={6} mt={5} p={5} rounded="lg" brightness="90%" borderColor="teal.700" maxW={800} _hover={{color: "teal.600", bg: "#eee", brightness: "100%", boxShadow: "dark-lg", transition: "transform 0.15s ease-in-out", transform: "scale3d(1.05, 1.05, 1.05)",}} style={{fontWeight: "400"}}>To mint TAUROS when the sale goes live, save your wallet address in the allowlist below. You may only mint 3 TAUROS per address! Price doubles for every 500 TAUROS tokens sold:
-                        </Text>
-                        <UnorderedList pt={3} ml={14}>
-                          <ListItem>Price from 1 to 500 tokens x 0.05 ETH</ListItem>
-                          <ListItem>Price from 501 to 1000 tokens x 0.1 ETH</ListItem>
-                          <ListItem>Price from 1001 to 1500 tokens x 0.2 ETH</ListItem>
-                          <ListItem>Price from 1501 to 2000 tokens x 0.4 ETH</ListItem>
-                        </UnorderedList>
-                        <Text pt={5}><span style={{fontWeight: "400"}}>Merca City</span> is an open world Metaverse/RTS Game, where TaurosDAO members, artists, designers, developers, gamers, and collectors may come together to play, experiment, and collaborate. <span style={{fontWeight: "400"}}>Estates</span> are Merca City&apos;s genesis assets. Out of 4300 Estates, 800 will be airdropped to TAUROS holders. 200 Estates will be reserved for special areas, public buildings, and institutions. The rest (3300) will be listed at a price of 0.1 ETH for the presale and 0.2 for the public sale.
-                        </Text>
-
-                        <Text pt={5}>The Estates NFT is dynamic, A.K.A. dNFT or <em>smart NFT</em>; its metadata and output animation changes based on live weather conditions pinpointing to a secret location. It reports temperature and visibility, and its animation depicts four weather conditions: sunny, overcast, rainy, and snowy.
-                        </Text> */}
-                        
                     </Collapse>
                     <Box align="right" px={4}>
                         <MotionIconButton variants={buttonVariants}  className={styles.arrow} w="50" h="50" onClick={onToggle} variant="unstyled" color="black" >
@@ -143,7 +125,7 @@ export default function Home() {
             <br /><br />
             Characters undergo a transformation as they move between worlds; thus, the character one plays is not exactly the same in both worlds, although there are similarities that carry across, for example a character&apos;s gait and other recognizable traits. Their identity becomes hidden under a mask in the Labyrinth. This is the perfect environment to form teams and factions that span both worlds, as well as infiltrate and undermine enemy factions.
             <br /><br />
-            <span style={{fontWeight: "400"}}>Initiation Masks</span> are the genesis assets of Labyrinthine Unreal, and the players&apos; ticket to the private alpha stage of the game. All OG Tauros holders will receive a free Initiation Mask for each token they own. Standard Tauros holders will be automatically whitelisted for the sale, and those who own both a Mask and an Estate will receive further upgrades to their assets in Merca City. Initiates will also be the first to receive the game&apos;s native token as rewards for completing tasks and/or solving various puzzles.
+            <span style={{fontWeight: "400"}}>Initiation Masks</span> are the genesis assets of Labyrinthine Unreal, and the players&apos; ticket to the private alpha stage of the game. As stated above, OGs have a 50% chance of receiving a free Initiation Mask for each token they own. Archons have a 25% chance and Guardians a 10% chance of receiving one. All TAUROS holders will be automatically whitelisted for the sale, and those who own both a Mask and an Estate may move freely between worlds. Initiates will also be the first to receive the game&apos;s native token as rewards for completing tasks and/or solving various puzzles. The Labyrinth&apos;s game token is different from that of Merca City.
         </Text>
     </Box>
   );
