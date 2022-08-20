2b0cc342066cef4c7e3816776bc11bf580444249
tectumor <72674429+tectumor@users.noreply.github.com>
2022-08-20 17:26:03 -0500

    triggeronce

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
 
   const [ref, inView] = useInView({
       threshold: 0.3,
-      triggerOnce: true
+      triggerOnce: false
   });
 
   const buttonVariants = {
