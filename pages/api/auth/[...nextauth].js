9027b810502068980720d4425ae7484e3525b92a
Calvin Green <calvingreen94@aol.com>
2022-09-08 17:47:10 -0500

    whats wrong with next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import CredentialsProvider from 'next-auth/providers/credentials';
-import NextAuth from 'next-auth/next';
+import NextAuth from 'next-auth';
 import Moralis from 'moralis';
 
 export default NextAuth({
