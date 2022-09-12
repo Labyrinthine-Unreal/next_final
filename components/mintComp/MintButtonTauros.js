36602f0dc15ae5195d6f0e2c7d12178bb7462943
tectumor <72674429+tectumor@users.noreply.github.com>
2022-09-12 11:17:19 -0500

    replace buttons with form

--- components/mintComp/MintButtonTauros.js
+++ components/mintComp/MintButtonTauros.js
@@ -1,4 +1,4 @@
-import { Link, Box, Button, Text } from "@chakra-ui/react"
+import { Link, Box, Button, Text, FormControl, FormLabel, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, Spacer } from "@chakra-ui/react"
 import { useEffect, useState, } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { connect } from "@/src/redux2/blockchain/blockchainActions"
@@ -65,21 +65,21 @@ export default function MintButtonTauros({ user }) {
           });
       };
 
-      const decrementMintAmount = () => {
-        let newMintAmount = mintAmount - 1;
-        if (newMintAmount < 1) {
-          newMintAmount = 1;
-        }
-        setMintAmount(newMintAmount);
-      };
+        const decrementMintAmount = () => {
+          let newMintAmount = mintAmount - 1;
+          if (newMintAmount < 1) {
+            newMintAmount = 1;
+          }
+          setMintAmount(newMintAmount);
+        };
 
-      const incrementMintAmount = () => {
-        let newMintAmount = mintAmount + 1;
-        if (newMintAmount > 10) {
-          newMintAmount = 10;
-        }
-        setMintAmount(newMintAmount);
-      };
+        const incrementMintAmount = () => {
+          let newMintAmount = mintAmount + 1;
+          if (newMintAmount > 5) {
+            newMintAmount = 5;
+          }
+          setMintAmount(newMintAmount);
+        };
 
       const getData = () => {
         if (blockchain.account !== "" && blockchain.smartContract !== null) {
@@ -132,27 +132,20 @@ export default function MintButtonTauros({ user }) {
             <Text textAlign="center">
               {feedback}
             </Text>
-            <Box>
-              <Button lineHeight="3" disabled={claimingNft ? 1 : 0}
-                onClick={(e) => {
-                  e.preventDefault();
-                  decrementMintAmount();
-                }}
-              >
-                -
-              </Button>
-              <Text textAlign="center">
-                {mintAmount}
-              </Text>
-              <Button disabled={claimingNft ? 1 : 0}
-                onClick={(e) => {
-                  e.preventDefault();
-                  incrementMintAmount();
-                }}
-              >
-                +
-              </Button>
-            </Box>
+            <FormControl my="4" maxW="210" minW="210">
+              <FormLabel htmlFor="amount" textAlign="right">
+                Amount to Mint
+              </FormLabel>
+
+              <NumberInput step={1} min={1} max={5} allowMouseWheel>
+                <NumberInputField id="amount" bg="gray.200" boxShadow="lg" />
+                <NumberInputStepper bg="teal.300">
+                  <NumberIncrementStepper borderLeft="none" onChange={incrementMintAmount} />
+                  <Spacer />
+                  <NumberDecrementStepper borderLeft="none" onChange={decrementMintAmount} />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
             <Box>
               <Button
                 disabled={claimingNft ? 1 : 0}
@@ -170,3 +163,142 @@ export default function MintButtonTauros({ user }) {
     </Box>
   )
 }
+
+
+// import { useToast, Link,Button, NumberInputStepper, Box, Spacer, NumberIncrementStepper, NumberDecrementStepper, NumberInputField, Text, FormControl, FormLabel, NumberInput } from "@chakra-ui/react"
+// import { useEffect, useState, } from "react";
+// import CustomContainer from "@components/CustomContainer";
+// import styles from "@styles/MintButton.module.css"
+// import taurosABI from "../ABIs/taurosABI"
+// import { usePrepareContractWrite, useWaitForTransaction } from 'wagmi'
+// import { ethers } from "ethers";
+// import { useAccount, useFeeData, useConnect, useSignMessage, useDisconnect } from 'wagmi';
+// import { useContractRead, useContractWrite } from 'wagmi';
+// import bigInt, { BigNumber } from "big-integer";
+
+
+// const truncate = (input, len) =>
+//   input.length > len ? `${input.substring(0, len)}...` : input;
+
+
+// export default function MBT() {
+//   const { address } = useAccount()
+//   const [amount, setAmount] = useState(1)
+//   const MINT_PRICE = 0.05;
+//   const MAX_MINT = 5;
+//   const handleChange = (value) => setAmount(value)
+//   const toast = useToast()
+
+//   const { data: PRICE } = useContractRead({
+//     addressOrName: '0xa58Ad36d83e45b068864Ef2f74Ce951DCB3930aA',
+//     contractInterface: taurosABI,
+//     functionName: 'PRICE',
+//   });
+
+//   const contractConfig = {
+//     addressOrName: '0xa58Ad36d83e45b068864Ef2f74Ce951DCB3930aA',
+//     contractInterface: taurosABI,
+//     functionName: 'claimTauros',
+//     args: [amount, { value: PRICE?.toString() }],
+//   };
+
+
+//   const { config } = usePrepareContractWrite({
+//     ...contractConfig
+//   })
+
+//   const { writeAsync: mint, error: mintError } = useContractWrite({
+//     ...config,
+//   });
+
+//   const [mintLoading, setMintLoading] = useState(false);
+//   const isConnected = !!address;
+//   const [mintedTokenId, setMintedTokenId] = useState(1);
+
+//   let onMintClick = async () => {
+//     try {
+//       setMintLoading(true);
+//       if (isConnected){
+//       const tx = await mint({
+//         args: [
+//           {
+//             gasLimit: 250000
+//           },
+//         ],
+//       });
+//       const receipt = await tx.wait();
+//       console.log('TX receipt', receipt);
+      
+//       const mintedTokenId = await receipt.events[0].args[2].toString();
+//       setMintedTokenId(mintedTokenId); 
+//       console.log(setMintedTokenId(mintedTokenId))
+//     }
+//     } catch (error) {
+//       console.error('error');
+//     } finally {
+//       setMintLoading(false);
+//     }
+//   };
+
+
+//     return (
+//       <CustomContainer>
+//         <Box fontSize="xl" fontWeight="bold" align="right">
+//           <form className={styles.btn} onSubmit={async e => {
+//             e.preventDefault()
+//           }}>
+//             <FormControl my="4" maxW="210" minW="210">
+//               <FormLabel htmlFor="amount" textAlign="right">
+//                 Amount to Mint
+//               </FormLabel>
+
+//               <NumberInput step={1} min={1} max={5} onChange={`${decrementMintAmount} ${incrementMintAmount}`} allowMouseWheel>
+//                 <NumberInputField id="amount" value={amount} bg="gray.200" boxShadow="lg" />
+//                 <NumberInputStepper bg="teal.300">
+//                   <NumberIncrementStepper borderLeft="none" />
+//                   <Spacer />
+//                   <NumberDecrementStepper borderLeft="none" />
+//                 </NumberInputStepper>
+//               </NumberInput>
+//             </FormControl>
+//             <Spacer />
+//             <Button
+//               disabled={!isConnected || mintLoading}
+//               marginTop='6'
+//               onClick={onMintClick}
+//               textColor='white'
+//               bg='blue.500'
+//               _hover={{
+//                 bg: 'blue.700',
+//               }}
+//             >
+//               {isConnected ? '🎉 Mint' : '🎉 Mint (Connect Wallet)'}
+//             </Button>
+//             {mintError && (
+//               <Text marginTop='4'>⛔️ Mint unsuccessful! Error message:</Text>
+//             )}
+//             {mintError && (
+//               <pre style={{ marginTop: '8px', color: 'red' }}>
+//                 <code>{JSON.stringify(mintError, null, ' ')}</code>
+//               </pre>
+//             )}
+//             {mintLoading && <Text marginTop='2'>Minting... please wait</Text>}
+//             {mintedTokenId && (
+//               <Text marginTop='2'>
+//                 🥳 Mint successful! You can view your NFT{' '}
+//                 <Link
+//                   isExternal
+//                   href='https://opensea.io'
+//                   color='blue'
+//                   textDecoration='underline'
+//                 >
+//                   here!
+//                 </Link>
+//               </Text>
+//             )}
+
+//           </form>
+//         </Box>
+//       </CustomContainer>
+//     )
+//   }
