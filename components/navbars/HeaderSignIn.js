7f2d46a27b32d4e9b787e3c445688bfc0e25c82b
tectumor <72674429+tectumor@users.noreply.github.com>
2022-09-17 12:19:18 -0500

    fix cannot read properties of null substring

--- components/navbars/HeaderSignIn.js
+++ components/navbars/HeaderSignIn.js
@@ -1,9 +1,9 @@
-import { Flex, Spacer } from '@chakra-ui/react'
-import HeaderLogo from './HeaderLogo'
+import { Flex, Spacer, Center, IconButton, Image } from '@chakra-ui/react'
 import Wallets from '../wallets/renderWallets'
+import { ImMenu } from 'react-icons/im'
 
 
-export default function HeaderSignIn({ ...rest }) {
+export default function HeaderSignIn({ onOpen, ...rest }) {
 
   return (
 
@@ -18,7 +18,19 @@ export default function HeaderSignIn({ ...rest }) {
       justifyContent={{ base: "space-between", md: "flex-end" }}
       {...rest}
     >
-      <HeaderLogo />
+      <Flex>
+      <IconButton
+        display={{ base: "flex", md: "none" }}
+        onClick={onOpen}
+        variant="outline"
+        aria-label="open menu"
+        icon={<ImMenu />}
+      />
+      <Center display={{ base: "none", md: "flex" }}>
+        <Image src="images/logos-icons/tauros_head.png" alt="Tauros_final" w="65px" h="85px" />
+        <Image pl={1} src="images/logos-icons/tauros_letters.png" alt="Tauros_final" w="180px" h="65px" />
+      </Center>
+    </Flex>
       <Spacer />
       <Wallets />
 
