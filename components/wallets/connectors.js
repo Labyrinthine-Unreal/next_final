b401326a081b8b47f7c1753c83b2db037bf8df55
Calvin Green <calvingreen94@aol.com>
2022-09-19 17:38:43 -0500

    Update connectors.js

--- components/wallets/connectors.js
+++ components/wallets/connectors.js
@@ -6,7 +6,7 @@ export const WalletConnect = {provider: 'walletconnect'}
 
 UAuthMoralisConnector.setUAuthOptions({
   clientID: "524a7dd4-bbd6-4633-9257-a685979aef44",
-  redirectUri: "http://localhost:3000",
+  redirectUri: "https://taurosdao.app/",
 
   // Scope must include openid and wallet
   scope: 'openid wallet',
