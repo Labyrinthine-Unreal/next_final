de122eb20122f75a1ac8f8f9cb559331d6649b72
tectumor <72674429+tectumor@users.noreply.github.com>
2022-09-21 16:46:32 -0500

    domain name display

--- components/wallets/DisconnectButton.js
+++ components/wallets/DisconnectButton.js
@@ -1,12 +1,18 @@
-import { Box, Center, Button, Icon, Menu, MenuButton, MenuList, MenuGroup, MenuItem, MenuDivider } from '@chakra-ui/react'
+import { Flex, Center, Button, Icon } from '@chakra-ui/react'
 import styles from '@styles/SignIn.module.css'
 import { useMoralis } from "react-moralis"
 import { AiOutlineWallet } from 'react-icons/ai'
+import { useEffect, useState, } from "react";
+import { UAuthMoralisConnector } from '@uauth/moralis';
 
 
 export default function Disconnect() {
-  const { isAuthenticated, authenticate, account, logout, isLoggingOut } = useMoralis()
-
+  const { refetchUserData, isInitialized, isUserUpdating, userError, isAuthenticated, authenticate,Moralis, account, user,logout, isLoggingOut } = useMoralis()
+  
+  // Displays the change of the UD domain name @theConnectedUser, or wallet address.
+  const [theConnectedUser, setConnectedUser] = useState();
+  const [shortWallet, setWalletAddress] = useState();
+  
   // DISCONNECT
   async function handleDisconnect() {
     try {
@@ -16,17 +22,38 @@ export default function Disconnect() {
     }
   }
 
-  // Shorten Address Display
-  const getShortenAddress = account => {
-    if (typeof account === "string") {
-      const firstCharacters = account.substring(0, 6)
-      const lastCharacters = account.substring(account.length - 4, account.length)
-      return `${firstCharacters}...${lastCharacters}`
+  function getUdUserInfo() {
+    try {
+      const uAuthMoralisConnector = new UAuthMoralisConnector()
+      uAuthMoralisConnector.uauth.user().then((user) => {
+        // Sets useState of UD domain name.
+        setConnectedUser(user.sub);
+      }).catch((error) => {
+        console.log(error)
+      });
+    } catch (error) {
+      console.log(error)
     }
   }
 
+  // Shorten Address Display
+  function getShortenAddress(account) {
+    if (typeof account === "string") {
+      const firstCharacters = account.substring(0, 6)
+      const lastCharacters = account.substring(account.length - 4, account.length)
+      setWalletAddress(`${firstCharacters}...${lastCharacters}`);
+    }
+  }
+
+  useEffect(() => {
+    // Sets wallet display upon a change in account.
+    getShortenAddress(account);
+  }, [account])
+  
+if (isAuthenticated){
+  getUdUserInfo();
   return (
-    <Box className={styles.connect}>
+    <Flex className={styles.connect}>
         <Center>
             {account && <Center 
             fontSize={14} 
@@ -34,19 +61,23 @@ export default function Disconnect() {
             bg="#009688bb" 
             color="#fff" 
             border="1px" 
-            _hover={{bg: "teal.400"}} 
-            position="absolute" 
-            mr={60} 
-            h="40px" 
-            p={3} 
-            pr={10} 
+            _hover={{bg: "teal.400"}}  
+            position="absolute"
+            w="fit-content" 
+            h="40px"
+            right={110}
+            py={3}
+            pl={3}
+            pr={8}
             rounded="3xl"
             >
             <Icon display={{ base: "none", md: "flex" }} fontSize={17} fontWeight="semibold" mr={2} as={AiOutlineWallet} />
-            {getShortenAddress(account)}
+            {theConnectedUser ? theConnectedUser : shortWallet }
+            
             </Center>}
             <Button onClick={handleDisconnect}>Disconnect</Button>
         </Center>
-    </Box>
+    </Flex>
   )
+            }
 }
