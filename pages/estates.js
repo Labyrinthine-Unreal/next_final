80c684157a6bb9b876b18a3f9affeb2c82b33d75
tectumor <72674429+tectumor@users.noreply.github.com>
2022-10-04 14:25:24 -0500

    SEO keywords

--- pages/estates.js
+++ pages/estates.js
@@ -31,6 +31,7 @@ export default function EstatesPage() {
                 <title>
                     TaurosDAO | Estates
                 </title>
+                <meta name="description" content="Welcome to TaurosDAO" keywords="NFT, nftart, digitalart, digitalartist, mint, taurosdao, labyrinthine, game, cryptocurrency, crypto, ethereum, membership, art, gallery, 3D, airdrop, airdrops, virtual real estate, plots, estate, estates, mask, masks, initiation, collection, marketplace, metaverse, MMORTS, exploration, mystery, podcast, bitcoin, token, coin, altcoin, voting, dao" />
             </Head>
             <Heading textStyle="title" fontSize="3xl">
                 <Estates />
--- pages/events.js
+++ pages/events.js
@@ -18,6 +18,7 @@ export default function EventsPage() {
                 <title>
                     TaurosDAO | Events
                 </title>
+                <meta name="description" content="Welcome to TaurosDAO" keywords="NFT, nftart, digitalart, digitalartist, mint, taurosdao, labyrinthine, game, cryptocurrency, crypto, ethereum, membership, art, gallery, 3D, airdrop, airdrops, virtual real estate, plots, estate, estates, mask, masks, initiation, collection, marketplace, metaverse, MMORTS, exploration, mystery, podcast, bitcoin, token, coin, altcoin, voting, dao" />
             </Head>
 
             <Heading fontSize="3xl" textStyle="title">
