496a44b7054b4eab312995115789bc0ff5305c4e
tectumor <72674429+tectumor@users.noreply.github.com>
2022-10-08 11:40:16 -0500

    Matt Hendrick Podcast

--- components/data/podcastsData.js
+++ components/data/podcastsData.js
@@ -1,5 +1,14 @@
 const podcastsData = [
     {
+        title: '4th Episode: Matt Hendrick',
+        croppedDescription: (<>Sat, Oct 08 | Frontier...</>),
+        description: (<>Oct 08, 2022, 12 PM EST<br />Frontier Podcast<br /><br />Quadriplegic mouth painter/NFT artist, <a style={{color: "#2E7DAF"}} href='https://twitter.com/matthendrickart' target="blank">Matt Hendrick</a>. When Matt's spinal cord was damaged in a catastrophic vehicle accident in 1991, he lost the ability to use his hands. These days, he likes oil painting and independent publishing. He uses dictation software to write, and his mouth operates the brush when he paints</>),
+        buttonText: 'Watch Now',
+        imageUrl: 'images/podcasts/Matt.jpg',
+        imageAlt: 'Matt Hendrick',
+        externalUrl: 'https://youtu.be/hcv4A7eM2QU'
+    },
+    {
         title: '3rd Episode: Ignis',
         croppedDescription: (<>Fri, Jul 29 | Frontier...</>),
         description: (<>Jul 29, 2022, 12 PM EST<br />Frontier Podcast<br /><br />Discussing the importance of art in culture and the artist&apos;s interpretation of digital fantasy. With digital artist and filmmaker, <a style={{color: "#2E7DAF"}} href='https://twitter.com/ignis____' target="blank">Ignis</a></>),
