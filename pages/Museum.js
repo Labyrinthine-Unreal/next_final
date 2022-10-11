47c4179cbe8ef714e15a43555bd3232529e787b7
Calvin Green <calvingreen94@aol.com>
2022-10-10 23:28:57 -0500

    Add files via upload

--- pages/Museum.js
+++ pages/Museum.js
@@ -1,30 +1,51 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { AiOutlineFullscreen } from "react-icons/ai";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import Button from "@chakra-ui/react";
+// import { useMoralis, useWeb3ExecuteFunction } from 'react-moralis';
+import { useEffect, useState, useRef } from "react";
+import { Box, Heading, Spacer, Text, Flex, SimpleGrid } from '@chakra-ui/react'
 
 // import "./BetaPage.css";
 
 export default function MuseumPage() {
-    const{ unityProvider, isLoaded, loadingProgression }  = useUnityContext({
-        loaderUrl: "../public/unity/build/WebGL/WebGL/Build/UnityLoader.js",
-        dataUrl: "../public/unity/build/WebGL/WebGL/Build/example.data.unityweb",
-        frameworkUrl: "../public/unity/build/WebGL/WebGL/Build/example.wasm.framework.unityweb",
-        codeUrl: "../public/unity/build/WebGL/WebGL/Build/example.wasm.code.unityweb",
-      });
-      const loadingPercentage = Math.round(loadingProgression * 100);
+    const { unityProvider, isLoaded, requestFullscreen, loadingProgression } = useUnityContext({
+        loaderUrl: "unity/build/WebGL/WebGL/Build/test3.loader.js",
+        dataUrl: "unity/build/WebGL/WebGL/Build/test3.data",
+        frameworkUrl: "unity/build/WebGL/WebGL/Build/test3.framework.js",
+        codeUrl: "unity/build/WebGL/WebGL/Build/test3.wasm",
+    });
 
-      return (
-        <div className="container">
-          {isLoaded === false && (
-            // We'll conditionally render the loading overlay if the Unity
-            // Application is not loaded.
-            <div className="loading-overlay">
-              <p>Loading... ({loadingPercentage}%)</p>
-            </div>
-          )}
-          <Unity className="unity" unityProvider={unityProvider} style={{ width: 800, height: 600 }}/>
-        </div>
-      );
+    function handleClickEnterFullscreen() {
+        requestFullscreen(true);
     }
+
+    const loadingPercentage = Math.round(loadingProgression * 100);
+    //   const { authenticate, isAuthenticated, isAuthenticating, Moralis, user, account, logout } = useMoralis();
+
+    //   useEffect(() => {
+    //     if (isAuthenticated) {
+
+    //     }
+
+    //   }, [isAuthenticated])
+
+    //   if (isAuthenticated){
+    return (
+        <Fragment>
+            <Box pt={10} align="center">
+                {isLoaded === false && (
+
+                    <div className="loading-overlay">
+                        <p>Loading... ({loadingPercentage}%)</p>
+                    </div>
+                )}
+                <button onClick={handleClickEnterFullscreen}><Text>Enter <br />Fullscreen</Text></button>
+                <Spacer /> <br />
+                <Unity className="unity" unityProvider={unityProvider} style={{ width: 900, height: 600 }} />
+            </Box>
+        </Fragment>
+    );
+}
+// }
 
