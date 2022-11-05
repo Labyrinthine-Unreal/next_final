b28bd923b90ebbc0aaa1eefc9b0e4af8bccf8418
Calvin Green <calvingreen94@aol.com>
2022-11-04 19:30:58 -0500

    pre-publish

--- pages/Museum.js
+++ pages/Museum.js
@@ -3,11 +3,11 @@ import { AiOutlineFullscreen } from "react-icons/ai";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import { useMoralis, useWeb3ExecuteFunction } from 'react-moralis';
 import { useEffect, useState, useRef } from "react";
-import {Heading, Flex, SimpleGrid} from '@chakra-ui/react';
+import { Heading, Flex, SimpleGrid } from '@chakra-ui/react';
 import styles from "@styles/MintButton.module.css";
 import taurosABI from "../components/ABIs/taurosABI";
 import CustomContainer from "@components/CustomContainer";
-import {useToast, NumberInputStepper, Box, Button, Spacer, NumberIncrementStepper, NumberDecrementStepper, NumberInputField, Text, FormControl, FormLabel, NumberInput} from "@chakra-ui/react"
+import { useToast, NumberInputStepper, Box, Button, Spacer, NumberIncrementStepper, NumberDecrementStepper, NumberInputField, Text, FormControl, FormLabel, NumberInput } from "@chakra-ui/react"
 
 // import "./BetaPage.css";
 
@@ -21,52 +21,52 @@ export default function MuseumPage() {
         contractAddress: "0x1A0F33bBc5c7bA83f490cdB6C13ee50e1C851908",
         functionName: "PRICE",
         abi: taurosABI,
-      };
+    };
 
-      useEffect(() => {
+    useEffect(() => {
         if (isAuthenticated) {
-    
-        }
-    
-      }, [isAuthenticated])
 
-      async function _mintTauros() {
+        }
+
+    }, [isAuthenticated])
+
+    async function _mintTauros() {
         let options = {
-          contractAddress: '0x1A0F33bBc5c7bA83f490cdB6C13ee50e1C851908',
-          functionName: 'claimTauros',
-          abi: taurosABI,
-          msgValue: await Moralis.executeFunction(PRICE) * amount,
-          // Moralis.Units.ETH("0.05")* amount,
-    //       Moralis.Units.ETH("0.08")
-          params: {
-            _count: amount,
-          }
+            contractAddress: '0x1A0F33bBc5c7bA83f490cdB6C13ee50e1C851908',
+            functionName: 'claimTauros',
+            abi: taurosABI,
+            msgValue: await Moralis.executeFunction(PRICE) * amount,
+            // Moralis.Units.ETH("0.05")* amount,
+            //       Moralis.Units.ETH("0.08")
+            params: {
+                _count: amount,
+            }
         }
 
         await contractProcessor.fetch({
             params: options,
             onSuccess: () => {
-              toast({
-                title: 'Mint Successful',
-                description: "Minted TAUROS",
-                status: 'success',
-                duration: 9000,
-                isClosable: true,
-              })
-              console.log("Mint successful");
+                toast({
+                    title: 'Mint Successful',
+                    description: "Minted TAUROS",
+                    status: 'success',
+                    duration: 9000,
+                    isClosable: true,
+                })
+                console.log("Mint successful");
             },
             onError: (error) => {
-              toast({
-                title: 'Mint Failed.. User rejected the transaction or not enough Ether To Purchase TAUROS',
-                description: console.log(error),
-                status: "error",
-                duration: '9000',
-                isClosable: true
-              })
-              console.log(error);
+                toast({
+                    title: 'Mint Failed.. User rejected the transaction or not enough Ether To Purchase TAUROS',
+                    description: console.log(error),
+                    status: "error",
+                    duration: '9000',
+                    isClosable: true
+                })
+                console.log(error);
             }
-          })
-        }
+        })
+    }
 
     const { unityProvider, isLoaded, requestFullscreen, loadingProgression } = useUnityContext({
         loaderUrl: "unity/build/WebGL/WebGL/Build/Gallery.loader.js",
@@ -74,7 +74,7 @@ export default function MuseumPage() {
         frameworkUrl: "unity/build/WebGL/WebGL/Build/Gallery.framework.js",
         codeUrl: "unity/build/WebGL/WebGL/Build/Gallery.wasm",
     });
-    
+
 
     function handleClickEnterFullscreen() {
         requestFullscreen(true);
@@ -83,12 +83,12 @@ export default function MuseumPage() {
     const loadingPercentage = Math.round(loadingProgression * 100);
     //   const { authenticate, isAuthenticated, isAuthenticating, Moralis, user, account, logout } = useMoralis();
 
-      useEffect(() => {
+    useEffect(() => {
         if (isAuthenticated) {
 
         }
 
-      }, [isAuthenticated])
+    }, [isAuthenticated])
 
     //   if (isAuthenticated){
     return (
@@ -105,41 +105,41 @@ export default function MuseumPage() {
                 <Spacer /> <br />
                 <Unity className="unity" unityProvider={unityProvider} style={{ width: 900, height: 600 }} />
             </Box>
-            <CustomContainer>
-      <Box fontSize="xl" fontWeight="bold" align="right">
-        <form className={styles.btn} onSubmit={async e => {
-          e.preventDefault()
-        }}>
-          <FormControl my="4" maxW="210" minW="210">
-            <FormLabel htmlFor="amount" textAlign="right">
-              Amount to Mint
-            </FormLabel>
+            {/* <CustomContainer> */}
+                <Box fontSize="xl" fontWeight="bold" align="right">
+                    <form className={styles.btn} onSubmit={async e => {
+                        e.preventDefault()
+                    }}>
+                        <FormControl my="4" maxW="210" minW="210">
+                            <FormLabel htmlFor="amount" textAlign="right">
+                                Amount to Mint
+                            </FormLabel>
 
-            <NumberInput step={1} min={1} max={10} defaultValue={1} onChange={handleChange} allowMouseWheel>
-              <NumberInputField  id="amount" value={amount} bg="gray.200" boxShadow="lg" />
-              <NumberInputStepper bg="teal.300">
-                <NumberIncrementStepper borderLeft="none" />
-                <Spacer />
-                <NumberDecrementStepper borderLeft="none" />
-                </NumberInputStepper>
-              </NumberInput>
-          </FormControl>
-          <Spacer />
-          <Button
-            color="white" 
-            _hover={{bg: "teal.400"}} 
-            rounded="xl"
-            onClick={() => {
-            if (isAuthenticated) { _mintTauros(); }
-          }}>
-            Mint
-          </Button>
+                            <NumberInput step={1} min={1} max={10} defaultValue={1} onChange={handleChange} allowMouseWheel>
+                                <NumberInputField id="amount" value={amount} bg="gray.200" boxShadow="lg" />
+                                <NumberInputStepper bg="teal.300">
+                                    <NumberIncrementStepper borderLeft="none" />
+                                    <Spacer />
+                                    <NumberDecrementStepper borderLeft="none" />
+                                </NumberInputStepper>
+                            </NumberInput>
+                        </FormControl>
+                        <Spacer />
+                        <Button
+                            color="white"
+                            _hover={{ bg: "teal.400" }}
+                            rounded="xl"
+                            onClick={() => {
+                                if (isAuthenticated) { _mintTauros(); }
+                            }}>
+                            Mint
+                        </Button>
 
-        </form>
-      </Box>
-    </CustomContainer>
+                    </form>
+                </Box>
+            {/* </CustomContainer> */}
         </Fragment>
-        
+
     );
 
 }
