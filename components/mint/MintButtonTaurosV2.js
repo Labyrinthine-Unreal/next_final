36709cc318b73b9bad184b96ba13d08104506399
Calvin Green <calvingreen94@aol.com>
2022-12-09 19:54:09 -0600

    minting  amount styling

--- components/mint/MintButtonTaurosV2.js
+++ components/mint/MintButtonTaurosV2.js
@@ -1,155 +1,195 @@
-import React, {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {fetchData} from "@src/redux/data/dataActions";
-import {Button, Container,Box, Link, Spacer, Text} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "@src/redux/data/dataActions";
+// import { Button, Container, Box, Link, Spacer, Text } from "@chakra-ui/react";
+import styles from "@styles/MintButton.module.css"
+import {useToast,Link, NumberInputStepper, Button, Box, Spacer, NumberIncrementStepper, NumberDecrementStepper, NumberInputField, Text, FormControl, FormLabel, NumberInput} from "@chakra-ui/react"
 
 const truncate = (input, len) =>
-    input.length > len ? `${input.substring(0, len)}...` : input;
+  input.length > len ? `${input.substring(0, len)}...` : input;
 
 export default function MBT() {
-    const dispatch = useDispatch();
-    const blockchain = useSelector((state) => state.blockchain);
-    const data = useSelector((state) => state.data);
-    const [claimingNft, setClaimingNft] = useState(false);
-    const [feedback, setFeedback] = useState(false);
-    const [mintAmount, setMintAmount] = useState(1);
-    
-    const [CONFIG, SET_CONFIG] = useState({
-        CONTRACT_ADDRESS: "",
-        SCAN_LINK: "",
-        NETWORK: {
-            NAME: "",
-            SYMBOL: "",
-            ID: 0,
-        },
-        NFT_NAME: "",
-        SYMBOL: "",
-        MAX_SUPPLY: 1,
-        WEI_COST: 0,
-        DISPLAY_COST: 0,
-        GAS_LIMIT: 0,
-        MARKETPLACE: "",
-        MARKETPLACE_LINK: "",
-        SHOW_BACKGROUND: false,
+  const dispatch = useDispatch();
+  const blockchain = useSelector((state) => state.blockchain);
+  const data = useSelector((state) => state.data);
+  const [claimingNft, setClaimingNft] = useState(false);
+  const [feedback, setFeedback] = useState(false);
+  const [mintAmount, setMintAmount] = useState(1);
+  // const [amount, setAmount] = useState(1)
+  const handleChange = (value) => setMintAmount(value)
+
+  const [CONFIG, SET_CONFIG] = useState({
+    CONTRACT_ADDRESS: "",
+    SCAN_LINK: "",
+    NETWORK: {
+      NAME: "",
+      SYMBOL: "",
+      ID: 0,
+    },
+    NFT_NAME: "",
+    SYMBOL: "",
+    MAX_SUPPLY: 1,
+    WEI_COST: 0,
+    DISPLAY_COST: 0,
+    GAS_LIMIT: 0,
+    MARKETPLACE: "",
+    MARKETPLACE_LINK: "",
+    SHOW_BACKGROUND: false,
+  });
+
+  const claimNFTs = () => {
+    let cost = CONFIG.WEI_COST;
+    let gasLimit = CONFIG.GAS_LIMIT;
+    let totalCostWei = String(cost * mintAmount);
+    let totalGasLimit = String(gasLimit * mintAmount);
+    console.log("Cost: ", totalCostWei);
+    console.log("Gas limit: ", totalGasLimit);
+    <Text>setFeedback(`Minting your ${CONFIG.NFT_NAME}...`)</Text>;
+    setClaimingNft(true);
+    blockchain.smartContract.methods
+      .mint(mintAmount) //mintAmount, "https://ipfs.io/ipfs/Qmf5ArUFQyuYBd591Ext3Z4WzXjWzxAd8vciF6dfbmC1P6?filename=gh.gif"
+      .send({
+        gasLimit: String(totalGasLimit),
+        to: CONFIG.CONTRACT_ADDRESS,
+        from: blockchain.account,
+        value: totalCostWei,
+      })
+      .once("error", (err) => {
+        console.log(err);
+        setFeedback(<Text>"Mint Failed"</Text>);
+        setClaimingNft(false);
+      })
+      .then((receipt) => {
+        console.log(receipt);
+        setFeedback(
+          <Text>`Nice , You are now a member of the ${CONFIG.NFT_NAME}`</Text>
+        );
+        setClaimingNft(false);
+        dispatch(fetchData(blockchain.account));
+      });
+  };
+
+  const decrementMintAmount = () => {
+    let newMintAmount = mintAmount - 1;
+    if (newMintAmount < 1) {
+      newMintAmount = 1;
+    }
+    setMintAmount(newMintAmount);
+  };
+
+  const incrementMintAmount = () => {
+    let newMintAmount = mintAmount + 1;
+    if (newMintAmount > 10) {
+      newMintAmount = 10;
+    }
+    setMintAmount(newMintAmount);
+  };
+
+  const getData = () => {
+    if (blockchain.account !== "" && blockchain.smartContract !== null) {
+      dispatch(fetchData(blockchain.account));
+    }
+  };
+
+  const getConfig = async () => {
+    const configResponse = await fetch("/config/config.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
     });
+    const config = await configResponse.json();
+    SET_CONFIG(config);
+  };
 
-    const claimNFTs = () => {
-        let cost = CONFIG.WEI_COST;
-        let gasLimit = CONFIG.GAS_LIMIT;
-        let totalCostWei = String(cost * mintAmount);
-        let totalGasLimit = String(gasLimit * mintAmount);
-        console.log("Cost: ", totalCostWei);
-        console.log("Gas limit: ", totalGasLimit);
-        <Text>setFeedback(`Minting your ${CONFIG.NFT_NAME}...`)</Text>;
-        setClaimingNft(true);
-        blockchain.smartContract.methods
-            .mint(mintAmount) //mintAmount, "https://ipfs.io/ipfs/Qmf5ArUFQyuYBd591Ext3Z4WzXjWzxAd8vciF6dfbmC1P6?filename=gh.gif"
-            .send({
-                gasLimit: String(totalGasLimit),
-                to: CONFIG.CONTRACT_ADDRESS,
-                from: blockchain.account,
-                value: totalCostWei,
-            })
-            .once("error", (err) => {
-                console.log(err);
-                setFeedback(<Text>"Mint Failed"</Text>);
-                setClaimingNft(false);
-            })
-            .then((receipt) => {
-                console.log(receipt);
-                setFeedback(
-                    <Text>`Nice , You are now a member of the ${CONFIG.NFT_NAME}`</Text>
-                );
-                setClaimingNft(false);
-                dispatch(fetchData(blockchain.account));
-            });
-    };
+  useEffect(() => {
+    getConfig();
+  }, []);
 
-    const decrementMintAmount = () => {
-        let newMintAmount = mintAmount - 1;
-        if (newMintAmount < 1) {
-            newMintAmount = 1;
-        }
-        setMintAmount(newMintAmount);
-    };
+  useEffect(() => {
+    getData();
+  }, [blockchain.account]);
+  console.log(blockchain.account)
 
-    const incrementMintAmount = () => {
-        let newMintAmount = mintAmount + 1;
-        if (newMintAmount > 10) {
-            newMintAmount = 10;
-        }
-        setMintAmount(newMintAmount);
-    };
+  return (
+    <Box>
+      <a href={CONFIG.MARKETPLACE_LINK}>
+      </a>
+      <Spacer />
 
-    const getData = () => {
-        if (blockchain.account !== "" && blockchain.smartContract !== null) {
-            dispatch(fetchData(blockchain.account));
-        }
-    };
-
-    const getConfig = async () => {
-        const configResponse = await fetch("/config/config.json", {
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-        });
-        const config = await configResponse.json();
-        SET_CONFIG(config);
-    };
-
-    useEffect(() => {
-        getConfig();
-    }, []);
-
-    useEffect(() => {
-        getData();
-    }, [blockchain.account]);
-    console.log(blockchain.account)
-
-    return (
-        <Box>
-            <a href={CONFIG.MARKETPLACE_LINK}>
-            </a>
-            <Spacer />
-
-            <Text>
-                {data.totalSupply} / {CONFIG.MAX_SUPPLY}
-            </Text>
-            <Text>
-                <Link target={"_blank"} href={CONFIG.SCAN_LINK}>
-                    {truncate(CONFIG.CONTRACT_ADDRESS, 15)}
-                </Link>
-            </Text>
-            { }
-            <Spacer />
-            {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? (
+      <Text>
+        {data.totalSupply} / {CONFIG.MAX_SUPPLY}
+      </Text>
+      <Text>
+        <Link target={"_blank"} href={CONFIG.SCAN_LINK}>
+          {truncate(CONFIG.CONTRACT_ADDRESS, 15)}
+        </Link>
+      </Text>
+      { }
+      <Spacer />
+      {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? (
+        <>
+          The sale has ended.
+          <Text>
+            You can still find {CONFIG.NFT_NAME} on
+          </Text>
+        </>
+      ) : (
+        <>
+          {blockchain.account === "" ||
+            blockchain.smartContract === null ? (
+            <Container>
+              {blockchain.errorMsg !== "" ? (
                 <>
-                    The sale has ended.
-                    <Text>
-                        You can still find {CONFIG.NFT_NAME} on
-                    </Text>
+                  <Text>
+                    {blockchain.errorMsg}
+                  </Text>
                 </>
-            ) : (
-                <>
-                    {blockchain.account === "" ||
-                        blockchain.smartContract === null ? (
-                        <Container>
-                            {blockchain.errorMsg !== "" ? (
-                                <>
-                                    <Text>
-                                        {blockchain.errorMsg}
-                                    </Text>
-                                </>
-                            ) : null}
-                        </Container>
-                    ) : (
-                        <>
+              ) : null}
+            </Container>
+          ) : (
+            <>
 
-                            {feedback}
+              {feedback}
 
-                            {/* Increase Mint Amount */}
-                            <Button
+              <Box fontSize="xl" fontWeight="bold" align="right">
+                <form className={styles.btn} onSubmit={async e => {
+                  e.preventDefault()
+                }}>
+                  <FormControl my="4" maxW="210" minW="210">
+                    <FormLabel htmlFor="amount" textAlign="right">
+                      Amount to Mint
+                    </FormLabel>
+
+                    <NumberInput step={1} min={1} max={10} defaultValue={1} onChange={handleChange} allowMouseWheel>
+                      <NumberInputField id="mintAmount" value={mintAmount} bg="gray.200" boxShadow="lg" />
+                      <NumberInputStepper bg="teal.300">
+                        <NumberIncrementStepper borderLeft="none" />
+                        <Spacer />
+                        <NumberDecrementStepper borderLeft="none" />
+                      </NumberInputStepper>
+                    </NumberInput>
+                  </FormControl>
+                  <Spacer />
+                  <Button
+                    disabled={claimingNft ? 1 : 0}
+                    onClick={(e) => {
+                      e.preventDefault();
+
+                      claimNFTs();
+                      getData();
+                    }}
+                  >
+                    <Text> {claimingNft ? "MINTING" : "MINT"}</Text>
+                  </Button>
+
+                </form>
+              </Box>
+
+
+
+              {/* Increase Mint Amount */}
+              {/* <Button
                                 disabled={claimingNft ? 1 : 0}
                                 onClick={(e) => {
                                     e.preventDefault();
@@ -157,21 +197,21 @@ export default function MBT() {
                                 }}
                             >
                                 ▲
-                            </Button>
-                            {/* Display Mint Amount */}
-                            <Spacer />
-                            {mintAmount}
-                            <Spacer />
-                            {/* Decrease Mint Amount */}
-                            <Button disabled={claimingNft ? 1 : 0} onClick={(e) => {
+                            </Button> */}
+              {/* Display Mint Amount */}
+              {/* <Spacer /> */}
+              {/* {mintAmount} */}
+              {/* <Spacer /> */}
+              {/* Decrease Mint Amount */}
+              {/* <Button disabled={claimingNft ? 1 : 0} onClick={(e) => {
                                 e.preventDefault();
                                 decrementMintAmount();
                             }}
                             >
                                 ▼
-                            </Button>
+                            </Button> */}
 
-                            <Button
+              {/* <Button
                                 disabled={claimingNft ? 1 : 0}
                                 onClick={(e) => {
                                     e.preventDefault();
@@ -181,14 +221,14 @@ export default function MBT() {
                                 }}
                             >
                                 <Text> {claimingNft ? "MINTING" : "MINT"}</Text>
-                            </Button>
-                        </>
-                    )}
-                </>
-            )}
-            <Spacer />
-            </Box>
-    );
+                            </Button> */}
+            </>
+          )}
+        </>
+      )}
+      <Spacer />
+    </Box>
+  );
 }
 
 
